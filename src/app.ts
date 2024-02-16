d956422ac8b4eb9630be228b26e4c2f5afe0dd0f
carlitossaul <carlitossaul.h@gmail.com>
2024-02-16 17:07:35 -0300

    Atualizar configurações do banco de dados e rotas

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-import express from 'express';
-import cors from 'cors';
-import loginRoutes from './routes/loginRoutes';
-import emailRoutes from './routes/emailRoutes';
-import statusRoutes from './routes/statusRoutes';
-import usuarioRoutes from './routes/usuarioRoutes';
-import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './swagger';
-import {sequelize} from './data-source';
+import express from "express";
+import cors from "cors";
+import loginRoutes from "./routes/loginRoutes";
+import emailRoutes from "./routes/emailRoutes";
+import statusRoutes from "./routes/statusRoutes";
+import usuarioRoutes from "./routes/usuarioRoutes";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./swagger";
+import { database } from "./data-source";
 
-
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,17 +18,19 @@ app.use(express.json());
 app.use(cors());
 
 function setupRoutes(): void {
-    app.use('/api/documentation', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-    app.use('/api/email', emailRoutes);
-    app.use('/api/', statusRoutes);
-    app.use('/api/', loginRoutes);
-    app.use('/api/', usuarioRoutes);
-};
+  app.use("/api/documentation", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api/email", emailRoutes);
+  app.use("/api/", statusRoutes);
+  app.use("/api", loginRoutes);
+  app.use("/api/usuario", usuarioRoutes);
+}
 
-sequelize.sync({ force: true }).then(() => {
-    console.log("Database and tables created!");
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-        return setupRoutes();
-    });
+database.sync({ force: true }).then(() => {
+  console.log(database.models);
+
+  console.log("Database and tables created!");
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    return setupRoutes();
+  });
 });
--- src/model/usuario_model.ts
+++ src/model/usuario_model.ts
@@ -1,69 +1,69 @@
-import { DataTypes, Model, ForeignKey, HasOne, HasMany } from 'sequelize';
-import Login from './login_model';
-import database from '../configs/sequelize.config';
+import { DataTypes, Model, ForeignKey, HasOne, HasMany } from "sequelize";
+import Login from "./login_model";
+import { database } from "../data-source";
 
 class Usuario extends Model {
-    public id!: number;
-    public name!: string;
-    public company_name!: string;
-    public login_id!: number;
+  public id!: number;
+  public name!: string;
+  public company_name!: string;
+  public login_id!: number;
 }
 
 Usuario.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [3, 45]
-            }
-        },
-        company_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-      },
-        login_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'login',
-                key: 'id'
-            }
-        },
-        created_at: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: new Date()
-        },
-        updated_at: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: new Date()
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize: database,
-        tableName: 'usuario',
-        modelName: 'Usuario',
-        timestamps: true,
-        createdAt: 'created_at',
-        updatedAt: 'updated_at',
-        scopes: {
-            defaultScope: {
-                attributes: { exclude: ['created_at', 'updated_at'] }
-            }
-        }
-    }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 45],
+      },
+    },
+    company_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    login_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "login",
+        key: "id",
+      },
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date(),
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date(),
+    },
+  },
+  {
+    sequelize: database,
+    tableName: "usuario",
+    modelName: "Usuario",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    scopes: {
+      defaultScope: {
+        attributes: { exclude: ["created_at", "updated_at"] },
+      },
+    },
+  }
 );
 
 Usuario.belongsTo(Login, {
-    foreignKey: 'login_id',
-    as: 'login'
+  foreignKey: "login_id",
+  as: "login",
 });
 
 export default Usuario;
