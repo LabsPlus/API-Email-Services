0397144aac32df64e60916a971e4b42e105d6c22
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-03-04 19:38:14 -0300

    Add email validation to sendEmail and adding tests coverage

--- src/controllers/emailController.ts
+++ src/controllers/emailController.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from 'express';
 import { EmailService } from '../services/emailService';
 import { Email } from "../interfaces/email/Imail";
+import EmailValidator from '../helpers/validators/email_validator';
 
 
 
 export default class EmailController {
     private emailService: EmailService;
-
+    private validateEmail: EmailValidator;
 
     constructor() {
         this.emailService = new EmailService();
-
+        this.validateEmail = new EmailValidator();
     }
 
     public async sendEmail(req: Request, res: Response) {
@@ -21,6 +22,14 @@ export default class EmailController {
                 return res.status(400).json({ error: 'Missing required parameters' });
             }
 
+            if (! await this.validateEmail.isEmailValid(to)) {
+                return res.status(400).json({ error: 'Invalid recipient e-mail address' });
+            }
+
+            if (! await this.validateEmail.isEmailValid(from)) {
+                return res.status(400).json({ error: 'Invalid sender e-mail address' });
+            }
+            
             const email = {
                 from,
                 subject,
