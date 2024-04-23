df78cdbe287f375906f2f05d3a7ee32152babf12
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-04-23 18:28:06 -0300

    Update validation logic in cpf_cnpj_validator.ts and email_validator.ts

--- src/helpers/validators/email_validator.ts
+++ src/helpers/validators/email_validator.ts
@@ -1,7 +1,7 @@
 export default class EmailValidator {
     
     public async isEmailValid(email: string): Promise <boolean> {
-        const emailRegex = /^w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         return emailRegex.test(email);
     }
 }
